bcf019ff5c782642581b2237b8d84f7e90681ee9
Wouter <wouter1602@gmail.com>
2020-05-04 16:38:47 +0200

    Added main.c file

---
+++ Project-AGV/Project-AGV/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project-AGV.c
+ *
+ * Created: 4-5-2020 16:36:46
+ * Author : Wouter
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
