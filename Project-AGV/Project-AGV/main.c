bd0e5c4eac7c628a7a5ecf1d35068b0c59231db1
Wouter <wouter1602@gmail.com>
2020-05-20 21:07:43 +0200

    Converted project to Aruidno Leonardo

--- Project-AGV/Project-AGV/main.c
+++ Project-AGV/Project-AGV/main.c
@@ -6,13 +6,20 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <USBCore.h>
 
 
-int main(void)
-{
+int main(void) {
+	sei();
+	
+	usbDeviceAttach();
+	DDRC |= (1 << DDC7);
     /* Replace with your application code */
-    while (1) 
-    {
+    while (1) {
+		PORTC ^= (1 << PORTC7);
+		_delay_ms(500);
     }
 }
 
