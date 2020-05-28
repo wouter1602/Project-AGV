85b83b6fe8a58a1aaa910001efaefb4de71e6a56
Evelien Blokdijk <evelienblokdijk@gmail.com>
2020-05-28 12:06:17 +0200

    Code Hall sensor is written for the first time

--- Project-AGV/Project-AGV/hall.c
+++ Project-AGV/Project-AGV/hall.c
@@ -3,14 +3,37 @@
  *
  * Created: 28-5-2020 09:18:49
  *  Author: Wouter
- */ 
+ */
 
 #include "hall.h"
-
-void initHall(void) {
-	
+void setupADC(void) {
+/*
+* setup ADC
+* PF0 A5 (ADC0)
+* MUX 0b000000 ADC0
+*/
+    DIDR0 |= (1 << ADC0D); //Disable digital inputs on ADC ports
+    ADMUX |= (1 << REFS0); //Select AREF as external reference
 }
 
-uint8_t getHallData(uint8_t sensor) {
-	
+void initHall(void){
+    setupADC();
+}
+//Read ADC for Current sensing
+static uint16_t readADCHall(void) {
+    ADMUX &= ~(1 << MUX0) & ~(1 << MUX1) & ~(1 << MUX2) & ~(1 << MUX3) & ~(1 << MUX4); //Reset MUX to 0
+    ADCSRB &= ~(1 << MUX5); //Reset MUX to 0
+    ADCSRA |= (1 << ADSC); //start measurement
+    while(!(ADCSRA & (1 << ADIF))); //wait for measurement
+    ADCSRA |= (1 << ADIF); //reset measurement flag
+    return ADC;
+}
+
+//This function checks if the AGV detects a magnet (the tree)
+//The code for not detecting the same magnet (tree) needs to be implemented in the main code (the routine the AGV executes when it has detected a magnet (tree)
+uint8_t getHallData(void){
+    if(HALL_THRESHOLD < readADCHall()){
+        return 1;
+    }
+    return 0;
 }
--- Project-AGV/Project-AGV/hall.h
+++ Project-AGV/Project-AGV/hall.h
@@ -3,7 +3,7 @@
  *
  * Created: 28-5-2020 09:19:46
  *  Author: Wouter
- */ 
+ */
 
 
 #ifndef HALL_H_
@@ -12,8 +12,11 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
+#include <avr/interrupt.h>
 
 #include "pins.h"
+
+#define HALL_THRESHOLD 0x7F
 
 void initHall(void);
 uint8_t getHallData(uint8_t sensor);
