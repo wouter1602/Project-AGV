16cfedaf2df9b4e42a95630cf2c1802549492a9f
JLiefrink <jochemliefrink97@gmail.com>
2020-06-22 23:51:49 +0200

    Navigation edit

--- Project-AGV/Project-AGV/navigation.c
+++ Project-AGV/Project-AGV/navigation.c
@@ -25,6 +25,42 @@ void setHeading(void) {
 	navigation.setHeading = getMagnetoHeading();
 }
 
+void calculateDeflection(void) {
+	int sum = 0;
+	for(i = 0; i < 5; i++)
+    {
+        sum = sum + memory[i];
+    }
+	AvarageAngle = sum/5;
+	deflection = AvarageAngle - navigation.setHeading;
+}
+
 void navigate(void) {
-	
+	while (1){
+		j = j++;
+		memory[j] = getMagnetoHeading;
+		if(j > 4){
+			j = j - 5;
+		}
+		if (deflection > 2){
+			TurnL(0x7F);
+			delay(1);
+			drive(0x7F);
+		}
+		else if(deflection < 358 && deflection > 270){
+			TurnR(0x7F);
+			delay(1);
+			drive(0x7F);
+		}
+		else{
+			drive(0x7F);
+		}
+	}
+}
+
+void navigateSetup(void) {
+	int memory[5];
+	for (i = 0; i < 5; i++){
+		memory[i] = getCurHeading;
+	}
 }
